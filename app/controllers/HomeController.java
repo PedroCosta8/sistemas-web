ef364d8d435d1e05a7fa7055c8252b202e8b7e53
Pedro <pedrobeethoven8@gmail.com>
2019-03-24 01:47:51 -0300

    pequenas modificações em arquivos

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,23 +8,23 @@ public class HomeController extends Controller {
     public Result index() {
         return ok(views.html.index.render());
     }
+    //
+    // public Result olaMundo(){
+    //   return ok("Ola Mundo!");
+    // }
+    //
+    // public Result somaPost() {
+    //   return ok(views.html.formSomaPost.render());
+    // }
+    //
+    // public Result formSomaPost() {
+    //   Map<String, String[]> r =
+    //       request().body().asFormUrlEncoded();
+    //   int a  = Integer.parseInt(r.get("a")[0]);
+    //   int b  = Integer.parseInt(r.get("b")[0]);
+    //
+    //   return ok(views.html.respostaSoma.render(a, b, a+b));
+    // }
 
-    public Result olaMundo(){
-      return ok("Ola Mundo!");
-    }
 
-    public Result somaPost() {
-      return ok(views.html.formSomaPost.render());
-    }
-
-    public Result formSomaPost() {
-      Map<String, String[]> r =
-          request().body().asFormUrlEncoded();
-      int a  = Integer.parseInt(r.get("a")[0]);
-      int b  = Integer.parseInt(r.get("b")[0]);
-
-      return ok(views.html.respostaSoma.render(a, b, a+b));
-    }
-
-    
 }
